8967c98bcbb137e55a98fac1d0cef8de9f870c21
Steven Pears <github@lotsofideas.co.uk>
2018-02-20 22:48:57 +0000

    Add initial local date methods and AmazonDate structure

---
+++ NodaTime.AmazonDate.Tests/LocalDateTimeTests.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Globalization;
+using Xunit;
+
+namespace NodaTime.AmazonDate.Tests
+{
+    public class LocalDateTimeTests
+    {
+        private const string LocalDateString = "2015-01-01";
+        private const string InvalidDateString = "abcd";
+
+        [Fact]
+        public void ParseValidLocalDate()
+        {
+            var result = AmazonDateParser.Parse(LocalDateString);
+            IsFirst2015(result.From);
+            IsFirst2015(result.To);
+        }
+
+        [Fact]
+        public void ParseInvalidLocalDate()
+        {
+            var result = AmazonDateParser.Parse(InvalidDateString);
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void TryParseValidLocalDate()
+        {
+            AmazonDate testResultDate;
+            var result = AmazonDateParser.TryParse(LocalDateString, out testResultDate);
+            Assert.True(result);
+            IsFirst2015(testResultDate.From);
+            IsFirst2015(testResultDate.To);
+        }
+
+        [Fact]
+        public void TryParseInvalidLocalDate()
+        {
+            AmazonDate testResultDate;
+            var result = AmazonDateParser.TryParse(InvalidDateString, out testResultDate);
+            Assert.False(result);
+            Assert.Null(testResultDate);
+        }
+
+        private void IsFirst2015(LocalDate date) => IsDate(date, 2015, 1, 1);
+
+        private void IsDate(LocalDate result, int year, int month, int day)
+        {
+            Assert.Equal(year, result.Year);
+            Assert.Equal(month,result.Month);
+            Assert.Equal(day, result.Day);
+        }
+    }
+}
---
+++ NodaTime.AmazonDate.Tests/StructureTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xunit;
+
+namespace NodaTime.AmazonDate.Tests
+{
+    public class StructureTests
+    {
+        [Fact]
+        public void EnsurePropertiesFit()
+        {
+            var from = new LocalDate(2015, 01, 01);
+            var to = new LocalDate(2016, 01, 01);
+            var date = new AmazonDate(from, to);
+            Assert.Equal(from, date.From);
+            Assert.Equal(to, date.To);
+        }
+    }
+}
